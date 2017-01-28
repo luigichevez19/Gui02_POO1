855dadd628312c2e35456dd4f79ee99347c9c6a4
Luis <Luis@Luis>
2017-01-28 12:21:36 -0600

    Ejercicios 1 y 2, el 2 casi echo pero el uno la clase falta

---
+++ Guia02_POO1/src/com/sv/udb/clases/Ejercicio1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.clases;
+
+/**
+ *
+ * @author Luis
+ */
+public class Ejercicio1 {
+
+    public int getNumBase() {
+        return numBase;
+    }
+
+    public void setNumBase(int numBase) {
+        this.numBase = numBase;
+    }
+
+    public int getNumPont() {
+        return numPont;
+    }
+
+    public void setNumPont(int numPont) {
+        this.numPont = numPont;
+    }
+    
+    int numBase;
+    int numPont;
+    
+    
+    
+}
---
+++ Guia02_POO1/src/com/sv/udb/clases/Ejercicio2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.clases;
+
+/**
+ *
+ * @author Luis
+ */
+public class Ejercicio2 {
+
+    private int cant;
+    private double denoDine; // Denominacion del dinero
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    public double getDenoDine() {
+        return denoDine;
+    }
+
+    public void setDenoDine(double denoDine) {
+        this.denoDine = denoDine;
+    }
+    
+         
+    
+}
